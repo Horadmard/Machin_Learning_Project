3c0205be7097b33a02d274f2ee1269d04fad1fe1
Horad <iranradmard1@gmail.com>
2024-02-01 14:29:48 +0330

    In progress

--- base.py
+++ base.py
@@ -1,18 +1,20 @@
 import numpy as np
 
-def predict(x, w, b): 
-    p = np.dot(x, w) + b     
-    return p    
+
+def predict(x, w, b):
+    p = np.dot(x, w) + b
+    return p
 
 
-def compute_cost(X, y, w, b): 
+def compute_cost(X, y, w, b):
     m = X.shape[0]
     cost = 0.0
-    for i in range(m):                                
+    for i in range(m):
         f_wb_i = np.dot(X[i], w) + b
         cost = cost + (f_wb_i - y[i])**2
     cost = cost / (2 * m)
     return cost
 
+
 b_init = 785.1811367994083
-w_init = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
+w_init = np.array([0.39133535, 18.75376741, -53.36032453, -26.42131618])
